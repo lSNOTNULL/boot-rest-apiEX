8d5758173ce98adba6ccbe5fac041dc2523f0de5
lSNOTNULL <iream1@naver.com>
2025-04-21 11:33:04 +0900

    기초

---
+++ src/main/java/org/example/bootrestapiex/BootRestApiExApplication.java
@@ -0,0 +1,13 @@
+package org.example.bootrestapiex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRestApiExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootRestApiExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bootrestapiex/BootRestApiExApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bootrestapiex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootRestApiExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
